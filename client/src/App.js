c328af321ce981030ade3334231c065a35b48261
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-09 17:50:12 +0100

    basic template

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
+      <div>Pages here</div>
+    </Router>
+  );
 }
 
 export default App;
+
+//navbar
+
+//feed
+//messages
+//discover
+//notifications
+//profile
